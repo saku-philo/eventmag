af4c9ebdd936a4b0b124d82cd231ff7fad347564
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-06-12 21:02:53 +0900

    event登録フォーム作成

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :place
+    validates :date
+    validates :start_at
+    validates :cost
+    validates :capacity
+  end
 end
