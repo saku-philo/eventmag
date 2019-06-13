1985ce5544cfd5b04ac39f6ae1500a09eafd6cda
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-06-13 12:48:52 +0900

    一覧表示機能,view作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
-  def index; end
+  def index
+    @events = Event.all
+  end
 
   def show; end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "step 1 create events"
+
+50.times do |n|
+  name = Faker::Restaurant.name
+  place = Faker::Address.city
+  image = ""
+  date = "2019-06-30"
+  start_at = "19:00"
+  end_at = "20:00"
+  link_url = Faker::Fillmurray.image
+  cost = Faker::Number.number(4)
+  capacity = Faker::Number.number(2)
+  meta = Faker::Marketing.buzzwords
+  Event.create!(name: name,
+                place: place,
+                image: image,
+                date: date,
+                start_at: start_at,
+                end_at: end_at,
+                link_url: link_url,
+                cost: cost,
+                capacity: capacity,
+                meta: meta
+               )
+end
