b8c01df1f0402e63e7faf691bd8568d499e40b82
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-06-17 14:49:07 +0900

    userとeventモデルの関連付け

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create edit destroy]
+  before_action :check_contributor, only: %i[edit update destroy]
+
   def index
     @events = Event.all
   end
@@ -12,7 +15,7 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.new(event_params)
     if @event.save
       redirect_to events_path, notice: "イベント「#{@event.name}」を登録しました！"
     else
@@ -20,12 +23,9 @@ class EventsController < ApplicationController
     end
   end
 
-  def edit
-    @event = Event.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @event = Event.find(params[:id])
     if @event.update(event_params)
       redirect_to events_path, notice: "イベント#{@event.name}の情報を更新しました！"
     else
@@ -34,7 +34,6 @@ class EventsController < ApplicationController
   end
 
   def destroy
-    @event = Event.find(params[:id])
     if @event.destroy
       redirect_to events_path, alert: "イベント#{@event.name}の情報を削除しました！"
     else
@@ -45,6 +44,14 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:name, :place, :date, :start_at, :end_at, :cost, :capacity, :meta)
+    params.require(:event).permit(:name, :place, :date, :start_at, :end_at, :cost, :capacity, :link_url, :meta)
+  end
+
+  def check_contributor
+    if current_user.id == params[:id]
+      @event = Event.find(params[:id])
+    else
+      redirect_to events_path, alert: "投稿者以外はイベントを編集、削除出来ません!"
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_031345) do
+ActiveRecord::Schema.define(version: 2019_06_17_012913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_06_16_031345) do
     t.text "meta"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
