3f9acd9c450ed11ea9fc6cafd38be6c9a9e677a5
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-06-18 20:34:19 +0900

    join controller修正

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,6 +1,6 @@
 class JoinsController < ApplicationController
   def create
-    @join = Join.new(user_id: 1, group_id: 6, is_leader: true)
+    @join = Join.new(user_id: current_user.id, group_id: @group.id)
     if @join.save
       redirect_to group_path(@group)
     else
