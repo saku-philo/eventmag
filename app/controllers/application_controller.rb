cfd37a6cca7239e76f09372a837c8368aa6df8fe
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-06-21 17:31:50 +0900

    ユーザーアイコン投稿機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # サインアップ時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    # アカウント編集の時にnameのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    # アカウント編集の時にname,iconのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name icon])
   end
 end
