5bd80e192b5553563cbab77018ce762512563a9e
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-06-22 08:48:21 +0900

    グループ招待機能作成

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,12 +1,22 @@
 class JoinsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    @join = Join.new(user_id: current_user.id, group_id: @group.id)
-    if @join.save
-      redirect_to group_path(@group)
+    group = Group.find(join_params[:group_id])
+    user = User.find_by(name: join_params[:user_name])
+    if user
+      @info = group.invite_member(user, group)
+      redirect_to group_path(group), notice: @info.to_s
     else
-      redirect_to new_group_path
+      redirect_to group_path(group), alert: "#{join_params[:user_name]}さんは未登録のようです。確認してください"
     end
   end
 
   def destroy; end
+
+  private
+
+  def join_params
+    params.permit(:group_id, :user_name)
+  end
 end
