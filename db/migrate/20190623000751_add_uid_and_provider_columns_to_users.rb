9b2f42b5587ff7bde2f4ac6f3024de07f3a29723
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-06-23 09:23:23 +0900

    OAuth認証によるユーザー作成機能作成

---
+++ db/migrate/20190623000751_add_uid_and_provider_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddUidAndProviderColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :uid, :string, default: "", null: false
+    add_column :users, :provider, :string, default: "", null: false
+    add_index :users, [:uid, :provider], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_074706) do
+ActiveRecord::Schema.define(version: 2019_06_23_000751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,8 +91,11 @@ ActiveRecord::Schema.define(version: 2019_06_21_074706) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "icon"
+    t.string "uid", default: "", null: false
+    t.string "provider", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "attends", "events"
