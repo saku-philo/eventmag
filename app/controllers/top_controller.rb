c30874db380123e7e9023c02a4a3b96427295427
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-08-31 12:58:53 +0900

    今日以降のイベントが表示されるよう修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   def index
     @q = Event.all.ransack(params[:q])
-    @events = @q.result(distinct: true).order(updated_at: 'DESC').limit(6)
+    @events = @q.result(distinct: true).where('date >= ?', Date.today).order(updated_at: 'DESC').limit(6)
     @groups = Group.order(updated_at: 'DESC').limit(5)
   end
 end
